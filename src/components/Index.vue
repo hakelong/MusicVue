e9051c528b11051d6215820cf6365607d0406917
hakelong <zydty5061@qq.com>
2018-03-07 13:44:19 +0800

    init

---
+++ src/components/Index.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="hello">
+    <img src="" alt="" id='img'>
+    sss
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  mounted(){
+
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.hello{
+  width: 100px;
+  color:red;
+}
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
